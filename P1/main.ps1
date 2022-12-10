f4c84b3a6ce1cd52ff0204ab390491f5b93446f5
Zofia Kubrak <zofiakubrak@gmail.com>
2022-12-10 14:52:23 +0100

    change to checking object type

--- P1/main.ps1
+++ P1/main.ps1
@@ -1,43 +1,46 @@
-$base_path = 'C:\Users\zocha\Documents\Projects\SYOP\P1\dane_testowe'
+$base_path = 'D:\Documents\Projects\SYOP\P1\dane_testowe'
 $ext = "pdf"
 
-
-$all =  ((Get-ChildItem -Path $base_path -Filter *.$ext -Recurse -File -Name) + (Get-ChildItem -Path $base_path -Directory -Recurse -Name)) | sort
-foreach($path in $all){
+$all = ((Get-ChildItem -Path $base_path -Filter *.$ext -Recurse -File -Name) + (Get-ChildItem -Path $base_path -Directory -Recurse -Name)) | Sort-Object
+if ((Get-ChildItem -Path $base_path -Filter *.$ext -Recurse -File -Name).Length -eq 0) {
+    Write-Host("Nic nie znaleziono") -ForegroundColor Green
+    return
+}
+foreach ($path in $all) {
+    $full_path = $base_path + "\" + $path
     $last = $path.LastIndexOf('\')
+    $is_file = (Get-Item $full_path) -is [System.IO.FileInfo]
+
     $i = 0 
-    if($last -ne -1){
+    if ($last -ne -1) {
         $i = 1
         $full_path = $base_path + "\" + $path
-        $size = (Get-Item -Path $full_path).Length
-        $lastModifiedDate = (Get-Item $full_path).LastWriteTime
-        $path = $path.Remove(0, $last+1)
+        if ($is_file) {
+            $size = (Get-Item -Path $full_path).Length
+            $lastModifiedDate = (Get-Item $full_path).LastWriteTime
+        }
+
+
+        $path = $path.Remove(0, $last + 1)
         $wciecie = ""
-        for($i=0; $i -le $last - 3; $i=$i+1 ){
+        for ($i = 0; $i -le $last - 3; $i = $i + 1 ) {
             $wciecie = " " + $wciecie
         }
-        for($i=$last - 3; $i -le $last-3; $i=$i+1 ){
-            $wciecie =  $wciecie + "'"
+        for ($i = $last - 3; $i -le $last - 3; $i = $i + 1 ) {
+            $wciecie = $wciecie + "'"
         }
-        for($i=$last - 2; $i -le $last; $i=$i+1 ){
-            $wciecie =  $wciecie + "-"
+        for ($i = $last - 2; $i -le $last; $i = $i + 1 ) {
+            $wciecie = $wciecie + "-"
         }
         $path = $wciecie + $path
-        $last_dot = $path.LastIndexOf('.')
     }
 
-    if($last_dot -ne -1 -and $i -ne 0){
-        $wciecie_podpis = ""
-        Write-Host($path)
-        $last_dash = $path.LastIndexOf(' ')
-        for($i=0; $i -le $last_dash + 4; $i=$i+1 ){
-            $wciecie_podpis = " " + $wciecie_podpis
-        }
-
-        Write-Host($wciecie_podpis+ $size + "b ") -ForegroundColor DarkGreen -NoNewline
+    if ( $is_file -eq "True") {
+        Write-Host($path) -ForegroundColor Red -NoNewline
+        Write-Host(" " + $size + "b ") -ForegroundColor DarkGreen -NoNewline
         Write-Host($lastModifiedDate) -ForegroundColor DarkCyan
     }
-    else{
-        Write-Host($path)
+    else {
+        Write-Host($path) -ForegroundColor Green
     }
 }
