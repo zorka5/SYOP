4fccce60bb9e6d65dc148afa08275687558e2510
Zofia Kubrak <zofiakubrak@gmail.com>
2022-12-31 16:24:06 +0100

    add dit searching

--- P2/main.ps1
+++ P2/main.ps1
@@ -7,7 +7,42 @@ $document = $word.Documents.Open($file_path)
 $document_content = $document.content.Text
 # $FirstParagraph = $document.Paragraphs[1].range.Text
 # $FirstParagraph
-Write-Host($document_content.GetType()) -ForegroundColor Red
+
+# Analiza zdań
+$end_o_sentence = @('.','!','?')
+$sentences = $document_content -join " "
+Write-Host($sentences.GetType())
+$characters = @{}
+foreach ($character in $end_o_sentence){
+    if($character -eq $end_o_sentence[0]){
+        $len_before = 0
+    }
+    else {
+        $len_before = $sentences.Length
+    }
+    $sentences = $sentences.Split($character)
+    $len_after = $sentences.Length
+    $diff = $len_after - $len_before
+    $characters.add($character, $diff)
+    Write-Host("Liczba zdan zakonczonych na '"+ $character + "': " + $diff + ". ")
+}
+# $sentences
+$characters
+$words = $sentences.Split(" ")
+$words_dict = @{}
+foreach ($word in $words){
+
+    if ($words_dict.ContainsKey($word)){
+        $words_dict.$word +=1
+    }
+    else{
+        $words_dict.Add($word, 0)
+    }
+}
+$words_dict
+Write-Host($words.Length) -ForegroundColor Red
+# $words
+$word_count = ($words).Length
 $sentences = $document_content.Split(".").Split("!").Split("?")
 Write-Host($sentences.GetType()) -ForegroundColor Red
 $document.close()
